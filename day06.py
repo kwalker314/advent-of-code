6d199f4064341674997f5e45cc3f772e4aa02cd0
Walker, Kristin N. (S&T-Student) <kristin.walker314@gmail.com>
2021-12-06 00:25:25 -0600

    slight refactor for part 2, it's much faster now

--- day06.py
+++ day06.py
@@ -1,25 +1,46 @@
 import numpy as np
 
-NEW_SPAWN_RATE = 9 #8+1 to account of timers being processed after spawn are added
+NEW_SPAWN_RATE = 8
 SPAWN_RATE = 6
 DAYS_1 = 80
 DAYS_2 = 256
 
-def spawnFish(fish: np.ndarray) -> np.ndarray:
-    newFish = [NEW_SPAWN_RATE] * len(fish[fish == 0])
-    currentFish = fish.copy()
-    currentFish[currentFish < 0] = SPAWN_RATE
-    return np.append(currentFish, newFish).astype(int)
-
-def processTimers(fish: np.ndarray) -> np.ndarray:
-    return fish - 1
+def processNumbers(fishNumbers):
+    newFish = fishNumbers[0]
+    i = 0
+    newNumbers = fishNumbers.copy()
+    while i < len(newNumbers)-1:
+        newNumbers[i] = newNumbers[i+1]
+        i += 1
+    newNumbers[SPAWN_RATE] += newFish
+    newNumbers[NEW_SPAWN_RATE] = newFish
+    return newNumbers
 
 if __name__ == '__main__':
     input = "inputs/input06.txt"
     fish = np.genfromtxt(input, delimiter=',', dtype=int)
     print(fish)
 
-    for i in range(DAYS_1):
-        fish = processTimers(spawnFish(fish))
+    # for each i in fishNumbers[i], the value at the index is
+    # how many fish have i more days until they spawn more fish
+    fishNumbers = [0] * (NEW_SPAWN_RATE+1)
+    for i in range(NEW_SPAWN_RATE):
+        fishNumbers[i] = len(fish[fish == i])
 
-    print(f'number of fish after {DAYS_1} days: {len(fish)}') #380612
+    for i in range(DAYS_1):
+        fishNumbers = processNumbers(fishNumbers)
+
+    sum_1 = 0
+    for num in fishNumbers:
+        sum_1 += num
+
+    print(f'sum: {sum_1}') #380612
+
+    for i in range(DAYS_1, DAYS_2):
+        fishNumbers = processNumbers(fishNumbers)
+
+    sum_2 = 0
+    for num in fishNumbers:
+        sum_2 += num
+
+    print(f'sum: {sum_2}')  # 1710166656900
