38ccbb8a0db3b23d1aace943a3064b229558f9f6
Walker, Kristin N. (S&T-Student) <kristin.walker314@gmail.com>
2021-12-11 14:22:39 -0600

    working through previous advents of code while i wait for the new one today ahaha

---
+++ 2020/day01.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+
+if __name__ == '__main__':
+    input_filename = "inputs\input" + os.path.basename(__file__)[-5:-3] + ".txt"
+    nums = np.genfromtxt(input_filename, dtype=np.int64, delimiter=4, autostrip=True)
+    winning_combo_1 = 0
+    winning_combo_2 = 0
+    while winning_combo_1 == 0 or winning_combo_2 == 0:
+        num = nums[0]
+        nums = nums[1:]
+        for addNum in nums:
+            if num+addNum == 2020 and winning_combo_1 == 0:
+                winning_combo_1 = num * addNum
+            for addNum2 in nums[1:]:
+                if num+addNum+addNum2 == 2020 and winning_combo_2 == 0:
+                    winning_combo_2 = num*addNum*addNum2
+    print(f'part 1: {winning_combo_1}') #787776
+    print(f'part 2: {winning_combo_2}') #262738554
---
+++ 2020/day02.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+
+DTYPE = [('lower_limit', np.int16), ('upper_limit', np.int16), ('letter', np.unicode_, 1), ('pw', np.dtype('U100'))]
+
+if __name__ == '__main__':
+    input_filename = "inputs\input" + os.path.basename(__file__)[-5:-3] + ".txt"
+    with open(input_filename) as f:
+        cleanedlines = (line.replace('-', ' ').replace(':', '') for line in f)
+        passwords = np.genfromtxt(cleanedlines, dtype=DTYPE, autostrip=True, delimiter=' ')
+    count_1 = 0
+    count_2 = 0
+    for password in passwords:
+        letter = password['letter']
+        pass_str = password['pw']
+        lower_lim = password['lower_limit']
+        upper_lim = password['upper_limit']
+        pos1 = lower_lim - 1
+        pos2 = upper_lim - 1
+
+        if lower_lim <= pass_str.count(letter) <= upper_lim:
+            count_1 += 1
+        if (0 <= pos1 < len(pass_str) and pass_str[pos1] == letter) != (0 <= pos2 < len(pass_str) and pass_str[pos2] == letter):
+            count_2 += 1
+    print(f'part 1: {count_1}') #439
+    print(f'part 2: {count_2}') #584
