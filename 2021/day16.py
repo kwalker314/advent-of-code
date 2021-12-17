0eefbc6d899a3b34d2c1011741040ff57c496d9a
Walker, Kristin N. (S&T-Student) <kristin.walker314@gmail.com>
2021-12-17 00:10:51 -0600

    day 16 part 1 - trying my best to keep up this year (this is the most days i've ever done!)

---
+++ 2021/day16.py
@@ -0,0 +1,84 @@
+INPUT = "inputs/input16.txt"
+VERSIONS_SUM = 0
+BITS = ''
+
+def add_to_version(add_ver):
+    global VERSIONS_SUM
+    VERSIONS_SUM += add_ver
+    return VERSIONS_SUM
+
+def initialize_bits():
+    global BITS
+    hex = open(INPUT).readline().strip()
+    hex_len = len(hex)*4
+    BITS = bin(int(hex, 16))[2:].rjust(hex_len, '0')
+    return BITS
+
+def has_BITS_left() -> bool:
+    return int(BITS, 2) != 0
+
+def trim_bits(new_start):
+    global BITS
+    BITS = BITS[new_start:]
+    return BITS
+
+def decode_type20() -> int: #length of bits of subpackets
+    bits_to_process = int(BITS[:15], 2)
+    trim_bits(15)
+    bits_length_at_start = len(BITS)
+    while bits_length_at_start - len(BITS) < bits_to_process and has_BITS_left():
+        _ = decode()
+    return 0
+
+def decode_type21() -> int: #number of subpackets
+    num_subpackets = int(BITS[:11], 2)
+    trim_bits(11)
+    for packet in range(num_subpackets):
+        if not has_BITS_left():
+            return
+        _ = decode()
+    return 0
+
+def decode_type_other() -> int:
+    length_type_id = BITS[0]
+    trim_bits(1)
+    if length_type_id == '0':
+        _ = decode_type20()
+    else:
+        _ = decode_type21()
+    return 0
+
+def decode_type4() -> int:
+    literal_val = ''
+    group = BITS[:5]
+    trim_bits(5)
+    while True:
+        literal_val += group[1:]
+        # all 5-bit groups of the literal start with a 1
+        # except the last group
+        if group[0] == '0':
+            break
+        else:
+            group = BITS[:5]
+            trim_bits(5)
+    return int(literal_val, 2)
+
+def decode() -> int:
+    type_id = decode_verAndTypeID()
+    if type_id == 4:
+        return decode_type4()
+    else:
+        return decode_type_other()
+
+def decode_verAndTypeID() -> int:
+    add_to_version(int(BITS[:3], 2))
+    type_id = int(BITS[3:6], 2)
+    trim_bits(6)
+    return type_id
+
+if __name__ == '__main__':
+    initialize_bits()
+    decode()
+
+    print(f'part 1: {VERSIONS_SUM}') #1002
+    print(f'part 2: {0}') #
