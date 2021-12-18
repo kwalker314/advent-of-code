620c7630b262d5f285443dfcbe5a8c3c563b3c1c
Walker, Kristin N. (S&T-Student) <kristin.walker314@gmail.com>
2021-12-17 18:17:52 -0600

    day 16 part 2 - HELL YEAH this worked perfectly on the first try! i've never done an intcode puzzle before so this is very exciting ^^

--- 2021/day16.py
+++ 2021/day16.py
@@ -22,31 +22,51 @@ def trim_bits(new_start):
     BITS = BITS[new_start:]
     return BITS
 
-def decode_type20() -> int: #length of bits of subpackets
+def compute_values(type_id: int, values: [int]) -> int:
+    if type_id == 0:
+        return sum(values)
+    elif type_id == 1:
+        product = 1
+        for value in values:
+            product *= value
+        return product
+    elif type_id == 2:
+        return min(values)
+    elif type_id == 3:
+        return max(values)
+    elif type_id == 5:
+        return 1 if values[0] > values[1] else 0
+    elif type_id == 6:
+        return 1 if values[0] < values[1] else 0
+    elif type_id == 7:
+        return 1 if values[0] == values[1] else 0
+
+def decode_operator_0(type_id: int) -> int: #length of bits of subpackets
     bits_to_process = int(BITS[:15], 2)
     trim_bits(15)
     bits_length_at_start = len(BITS)
+    values = []
     while bits_length_at_start - len(BITS) < bits_to_process and has_BITS_left():
-        _ = decode()
-    return 0
+        values.append(decode())
+    return compute_values(type_id, values)
 
-def decode_type21() -> int: #number of subpackets
+def decode_operator_1(type_id: int) -> int: #number of subpackets
     num_subpackets = int(BITS[:11], 2)
     trim_bits(11)
+    values = []
     for packet in range(num_subpackets):
         if not has_BITS_left():
             return
-        _ = decode()
-    return 0
+        values.append(decode())
+    return compute_values(type_id, values)
 
-def decode_type_other() -> int:
+def decode_operator(type_id: int) -> int:
     length_type_id = BITS[0]
     trim_bits(1)
     if length_type_id == '0':
-        _ = decode_type20()
+        return decode_operator_0(type_id)
     else:
-        _ = decode_type21()
-    return 0
+        return decode_operator_1(type_id)
 
 def decode_type4() -> int:
     literal_val = ''
@@ -68,7 +88,7 @@ def decode() -> int:
     if type_id == 4:
         return decode_type4()
     else:
-        return decode_type_other()
+        return decode_operator(type_id)
 
 def decode_verAndTypeID() -> int:
     add_to_version(int(BITS[:3], 2))
@@ -78,7 +98,7 @@ def decode_verAndTypeID() -> int:
 
 if __name__ == '__main__':
     initialize_bits()
-    decode()
+    final_value = decode()
 
     print(f'part 1: {VERSIONS_SUM}') #1002
-    print(f'part 2: {0}') #
+    print(f'part 2: {final_value}') #1673210814091
