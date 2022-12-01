3655553fec6e0d8bec5a3fdb53854a30a696c19a
Walker, Kristin N. (S&T-Student) <kristin.walker314@gmail.com>
2022-11-30 23:15:30 -0600

    2022 was a year of so many changes but i'm happy AoC is here, same as always :)

---
+++ 2022/day01.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    input = open("inputs/input01.txt")
+    sums = []
+    currSum = 0
+    for line in input:
+        if line == '\n':
+            sums.append(currSum)
+            currSum = 0
+        else:
+            currSum += int(line)
+    input.close()
+    sums.sort(reverse=True)
+    print("part 1: ", sums[0])
+    print("part 2: ", sum(sums[0:3]))
