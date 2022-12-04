1da348a9769fb52ccd464bd83c53b8590b6998d4
Walker, Kristin N. (S&T-Student) <kristin.walker314@gmail.com>
2022-12-03 23:17:50 -0600

    day 4 ez!

---
+++ 2022/day04.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+def parseRanges(ranges: str) -> (str, str):
+    ranges = ranges.split(',')
+    return (ranges[0], ranges[1])
+
+def checkContainsAndOverlap(range1: str, range2: str) -> (bool, bool):
+    range1Start, range1End = range1.split('-')
+    range2Start, range2End = range2.split('-')
+    range1Start, range1End = int(range1Start), int(range1End)
+    range2Start, range2End = int(range2Start), int(range2End)
+    rangeContained = (range1Start <= range2Start and range1End >= range2End) \
+        or (range2Start <= range1Start and range2End >= range1End)
+    #ty stackoverflow for this ez range overlap check!
+    rangeOverlap = max(range1Start, range2Start) <= min(range1End, range2End)
+    return (rangeContained, rangeOverlap)
+
+
+def checkRanges(line: str) -> (bool, bool):
+    range1, range2 = parseRanges(line)
+    return checkContainsAndOverlap(range1, range2)
+
+input = open(os.path.join(os.getcwd(), "inputs/input04.txt"))
+part1 = 0
+part2 = 0
+for line in input:
+    results = checkRanges(line.strip())
+    part1 += results[0]
+    part2 += results[1]
+input.close()
+assert part1 == 513, f'Part 1: expected 513 but got {part1}'
+assert part2 == 878, f'Part 2: expected 878 but got {part2}'
+print("part 1: ", part1)
+print("part 2: ", part2)
