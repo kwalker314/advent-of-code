cde755edeec6da661d3511d63e9eb69ec1e82d7d
Walker, Kristin N. (S&T-Student) <kristin.walker314@gmail.com>
2023-12-04 00:12:15 -0600

    refactored - much faster now!

--- 2023/day04.py
+++ 2023/day04.py
@@ -5,6 +5,7 @@ input = open(os.path.join(os.getcwd(), "inputs/input04.txt"))
 output1 = 0
 output2 = 0
 winningCards = {}
+cardsProducedDict = {}
 correctOutput = (20829, 12648035)
 cardsToProcess = []
 for line in input:
@@ -23,10 +24,20 @@ for line in input:
     output1 += pointVal
     cardsToProcess.append(cardNum)
 input.close()
+# for each card in the list, which we go through starting from the final card
+# (which inherently produced no additional cards), add up the sum of all
+# cards produced by this card (including itself) and memoize it in the
+# cardsProducedDict under this cardNum's index
+# (...this sounds like a YuGiOh card effect LOL)
 while len(cardsToProcess) > 0:
     cardNum = cardsToProcess.pop()
-    output2 += 1
-    cardsToProcess.extend(winningCards[cardNum])
+    numCardsProduced = 1 #card counts itself as a card it produced
+    for producedCardNum in winningCards[cardNum]:
+        numCardsProduced += cardsProducedDict[producedCardNum]
+    cardsProducedDict[cardNum] = numCardsProduced
+# finally, go back through all the cards in our dictionary and add up their values
+for winningCardNum in cardsProducedDict.keys():
+    output2 += cardsProducedDict[winningCardNum]
 assert output1 == correctOutput[0], f'Part 1: expected {correctOutput[0]} but got {output1}'
 assert output2 == correctOutput[1], f'Part 2: expected {correctOutput[1]} but got {output2}'
 print("part 1: ", output1)
